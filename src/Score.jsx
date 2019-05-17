0f31d4737378e1ee3de7e65a62156fc64271ce72
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-17 14:51:17 +0100

    fix css

--- src/Score.jsx
+++ src/Score.jsx
@@ -11,8 +11,10 @@ const Score = ( {score, handleInicialState} ) => {
                 <img src={isGameOver() ? "https://media.giphy.com/media/l0IyaF4KPk0clJWYE/giphy.gif" : "https://media.giphy.com/media/3og0IUEVx2NvH3iVj2/giphy.gif" } alt=""/>
             </div>
             <h2>{isGameOver() ? "" : `Your score is: ${score}`}</h2>
-            <Link to="/hangman" className="play" onClick={handleClick}><button>Play Again</button></Link>            
-            <Link to="/" className="back-home"><button>Back home</button></Link>   
+            <div className="score-button">
+                <Link to="/hangman" className="play" onClick={handleClick}><button>Play Again</button></Link>            
+                <Link to="/" className="back-home"><button>Back home</button></Link>   
+            </div>
         </div>
     );
 }

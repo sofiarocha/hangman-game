4a06de1331cdbce6f735e469bf9bf2ec23597007
Sofia <sofiaestanqueirorocha@gmail.com>
2019-05-17 14:23:09 +0100

    Updates

---
+++ precache-manifest.4aff88a7baa99bfe6d46a6634e96b6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0488932f026273b23bff98612d24efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b40cf21e4c37fd445c02",
+    "url": "/static/css/main.d8091eed.chunk.css"
+  },
+  {
+    "revision": "f7df3c5156292e0193ba",
+    "url": "/static/js/2.51d24f17.chunk.js"
+  },
+  {
+    "revision": "b40cf21e4c37fd445c02",
+    "url": "/static/js/main.19a35898.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
